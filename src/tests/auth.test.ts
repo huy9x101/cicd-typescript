d187cde43da936a2f935db45daffbb7a0aff5b2c
David Nguyen <huy9x101@gmail.com>
2026-06-24 08:17:45 +0700

    feat: Add suceed test

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -3,16 +3,11 @@ import { describe, expect, test } from "vitest";
 const person = {
   isActive: true,
   age: 32,
-  address: null,
 };
 
 describe("person", () => {
   test("person is defined", () => {
     expect(person).toBeDefined();
-  });
-
-  test("person address is defined", () => {
-    expect(person.address).toBeNaN();
   });
 
   test("is active", () => {
